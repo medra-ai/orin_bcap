2a07c9a4f65aea1d88e7548133059d9a383e887f
Paul Chen <paulchen@medra.ai>
2024-08-01 19:25:23 +0000

    Add ExecCurJnt and ExecSlaveMove and buids

--- medra_pybind/include/medra_denso_robot.hpp
+++ medra_pybind/include/medra_denso_robot.hpp
@@ -30,6 +30,51 @@ public:
     static constexpr int BCAP_ROBOT_EXECUTE_ARGS = 3;
     static constexpr int ARM_GROUP = 0;
 
+    // From denso_robot_core_interfaces::msg::UserIO
+    static constexpr int USERIO_ALIGNMENT=8;
+
+    enum
+    {
+    NUM_POSITION = 7,
+    NUM_JOINT = 8,
+    NUM_TRANS = 10
+    };
+
+    enum
+    {
+        SLVMODE_NONE = 0,
+        SLVMODE_POSE_P = 0x0001,
+        SLVMODE_POSE_J = 0x0002,
+        SLVMODE_POSE_T = 0x0003,
+        SLVMODE_POSE = 0x000F,
+        SLVMODE_ASYNC = 0x0100,
+        SLVMODE_SYNC_WAIT = 0x0200,
+    };
+
+    enum
+    {
+        SENDFMT_NONE = 0,
+        SENDFMT_HANDIO = 0x0020,
+        SENDFMT_MINIIO = 0x0100,
+        SENDFMT_USERIO = 0x0200,
+    };
+
+    enum
+    {
+        RECVFMT_NONE = 0,
+        RECVFMT_POSE_P = 0x0001,
+        RECVFMT_POSE_J = 0x0002,
+        RECVFMT_POSE_T = 0x0003,
+        RECVFMT_POSE_PJ = 0x0004,
+        RECVFMT_POSE_TJ = 0x0005,
+        RECVFMT_POSE = 0x000F,
+        RECVFMT_TIME = 0x0010,
+        RECVFMT_HANDIO = 0x0020,
+        RECVFMT_CURRENT = 0x0040,
+        RECVFMT_MINIIO = 0x0100,
+        RECVFMT_USERIO = 0x0200,
+    };
+
     // Constructor and destructor
     MedraDensoRobot(const std::string& name, const int* mode, const std::string& ip_address);
     ~MedraDensoRobot();
@@ -40,9 +85,21 @@ public:
     // Robot commands
     HRESULT ExecTakeArm();
     HRESULT ExecGiveArm();
-    // HRESULT ExecCurJnt(std::vector<double>& pose);
-    // HRESULT ExecSlaveMove(const std::vector<double>& pose, std::vector<double>& joint);
+
+    HRESULT ExecCurJnt(std::vector<double>& pose);
+    HRESULT ExecSlaveMove(const std::vector<double>& pose, std::vector<double>& joint);
+
 private:
+    HRESULT CreateSendParameter(
+        const std::vector<double>& pose, VARIANT_Ptr& send, const int miniio = 0,
+        const int handio = 0, const int recv_userio_offset = 0, const int recv_userio_size = 0,
+        const int send_userio_offset = 0, const int send_userio_size = 0,
+        const std::vector<uint8_t>& send_userio = std::vector<uint8_t>());
+    HRESULT ParseRecvParameter(
+        const VARIANT_Ptr& recv, std::vector<double>& position, std::vector<double>& joint,
+        std::vector<double>& trans, int& miniio, int& handio, int& timestamp,
+        std::vector<uint8_t>& recv_userio, std::vector<double>& current);
+
 
     BCAPService_Ptr _bcap_service;
     Handle _controller_handle;
@@ -50,6 +107,15 @@ private:
     std::string m_name;
     const int * m_mode;
     std::string m_addr;
+
+    int m_tsfmt, m_timestamp;
+    int m_sendfmt, m_send_miniio, m_send_handio;
+    int m_recvfmt, m_recv_miniio, m_recv_handio;
+    int m_send_userio_offset, m_send_userio_size;
+    int m_recv_userio_offset, m_recv_userio_size;
+    std::vector<uint8_t> m_send_userio, m_recv_userio;
+    std::vector<double> m_position, m_joint, m_trans, m_current;
+
 };
 
 
--- medra_pybind/src/medra_denso_robot.cpp
+++ medra_pybind/src/medra_denso_robot.cpp
@@ -15,6 +15,7 @@
 #include "medra_bcap_service.hpp"
 
 #include <sstream>
+#include <cstring>
 
 namespace medra_denso_robot {
 
@@ -159,6 +160,416 @@ HRESULT MedraDensoRobot::ExecGiveArm()
   }
 
   return _bcap_service->ExecFunction(ID_ROBOT_EXECUTE, vntArgs, vntRet);
+}
+
+// Based on DensoRobot::ExecCurJnt
+HRESULT MedraDensoRobot::ExecCurJnt(std::vector<double>& pose)
+{
+  HRESULT hr;
+
+  int argc;
+  VARIANT_Vec vntArgs;
+  VARIANT_Ptr vntRet(new VARIANT());
+
+  VariantInit(vntRet.get());
+
+  for (argc = 0; argc < BCAP_ROBOT_EXECUTE_ARGS; argc++) {
+    VARIANT_Ptr vntTmp(new VARIANT());
+    VariantInit(vntTmp.get());
+
+    switch (argc) {
+      case 0:
+        vntTmp->vt = VT_UI4;
+        vntTmp->ulVal = _controller_handle;
+        break;
+      case 1:
+        vntTmp->vt = VT_BSTR;
+        vntTmp->bstrVal = SysAllocString(L"HighCurJntEx");
+        break;
+    }
+
+    vntArgs.push_back(*vntTmp.get());
+  }
+
+  hr = _bcap_service->ExecFunction(ID_ROBOT_EXECUTE, vntArgs, vntRet);
+
+  if (SUCCEEDED(hr) && (vntRet->vt == (VT_ARRAY | VT_R8))) {
+    uint32_t num;
+    double* pdblval;
+
+    num = vntRet->parray->rgsabound->cElements;
+    SafeArrayAccessData(vntRet->parray, (void**)&pdblval);
+    pose.resize(num - 1);
+    std::copy(&pdblval[1], &pdblval[num], pose.begin());
+    SafeArrayUnaccessData(vntRet->parray);
+  }
+
+  return hr;
+}
+
+// Based on DensoRobot::ExecSlaveMove
+HRESULT MedraDensoRobot::ExecSlaveMove(const std::vector<double>& pose, std::vector<double>& joint)
+{
+  HRESULT hr = S_OK;
+  int argc;
+  VARIANT_Vec vntArgs;
+  VARIANT_Ptr vntRet(new VARIANT());
+
+  VariantInit(vntRet.get());
+
+  for (argc = 0; argc < BCAP_ROBOT_EXECUTE_ARGS; argc++) {
+    VARIANT_Ptr vntTmp(new VARIANT());
+    VariantInit(vntTmp.get());
+
+    switch (argc) {
+      case 0:
+        vntTmp->vt = VT_UI4;
+        vntTmp->ulVal = _controller_handle;
+        break;
+      case 1:
+        vntTmp->vt = VT_BSTR;
+        vntTmp->bstrVal = SysAllocString(L"slvMove");
+        break;
+      case 2:
+        hr = CreateSendParameter(pose, vntTmp, m_send_miniio, m_send_handio, m_recv_userio_offset, m_recv_userio_size,
+                                 m_send_userio_offset, m_send_userio_size, m_send_userio);
+        if (FAILED(hr)) {
+          return hr;
+        }
+        break;
+    }
+
+    vntArgs.push_back(*vntTmp.get());
+  }
+
+  hr = _bcap_service->ExecFunction(ID_ROBOT_EXECUTE, vntArgs, vntRet);
+  if (SUCCEEDED(hr)) {
+    HRESULT hrTmp = ParseRecvParameter(
+      vntRet, m_position, m_joint, m_trans, m_recv_miniio, m_recv_handio,
+      m_timestamp, m_recv_userio, m_current);
+
+    joint = m_joint;
+
+    if (FAILED(hrTmp)) {
+      hr = hrTmp;
+    }
+  }
+
+  return hr;
+}
+
+
+HRESULT MedraDensoRobot::CreateSendParameter(
+  const std::vector<double>& pose, VARIANT_Ptr& send, const int miniio,
+  const int handio, const int recv_userio_offset, const int recv_userio_size,
+  const int send_userio_offset, const int send_userio_size,
+  const std::vector<uint8_t>& send_userio)
+{
+  int type = *m_mode & SLVMODE_POSE;
+
+  // Check pose type
+  int joints = 0;
+  switch (type) {
+    case SLVMODE_POSE_P:
+      joints = NUM_POSITION;
+      break;
+    case SLVMODE_POSE_J:
+      joints = NUM_JOINT;
+      break;
+    case SLVMODE_POSE_T:
+      joints = NUM_TRANS;
+      break;
+    default:
+      return E_FAIL;
+  }
+
+  // TODO: do we need this control ? Why?
+  // if(joints < pose.size())
+  // {
+    // return E_FAIL;
+  // }
+
+  // Check send format
+  bool send_hio, send_mio, send_uio, recv_uio;
+  send_hio = m_sendfmt & SENDFMT_HANDIO;
+  send_mio = m_sendfmt & SENDFMT_MINIIO;
+  send_uio = m_sendfmt & SENDFMT_USERIO;
+
+  if (send_uio) {
+    if (static_cast<std::size_t>(send_userio_size) < send_userio.size()) {
+      return E_FAIL;
+    }
+  }
+
+  // Check receive format
+  recv_uio = m_recvfmt & RECVFMT_USERIO;
+
+  // Number of arguments
+  int num = 1 + send_hio + send_mio + 3 * send_uio + 2 * recv_uio;
+
+  VARIANT* pvnt;
+  double* pdbl;
+  uint8_t* pbool;
+
+  // Number of joints + option
+  joints += 1;
+  if (num == 1) {
+    // Pose only
+    send->vt = (VT_ARRAY | VT_R8);
+    send->parray = SafeArrayCreateVector(VT_R8, 0, joints);
+    SafeArrayAccessData(send->parray, (void**)&pdbl);
+    memset(pdbl, 0, joints * sizeof(double));
+    std::copy(pose.begin(), pose.end(), pdbl);
+    SafeArrayUnaccessData(send->parray);
+  } else {
+    send->vt = (VT_ARRAY | VT_VARIANT);
+    send->parray = SafeArrayCreateVector(VT_VARIANT, 0, num);
+
+    SafeArrayAccessData(send->parray, (void**)&pvnt);
+
+    int offset = 0;
+
+    // Pose
+    {
+      pvnt[offset].vt = (VT_ARRAY | VT_R8);
+      pvnt[offset].parray = SafeArrayCreateVector(VT_R8, 0, joints);
+      SafeArrayAccessData(pvnt[offset].parray, (void**)&pdbl);
+      memset(pdbl, 0, joints * sizeof(double));
+      std::copy(pose.begin(), pose.end(), pdbl);
+      SafeArrayUnaccessData(pvnt[offset].parray);
+
+      offset++;
+    }
+
+    // Mini I/O
+    if (send_mio) {
+      pvnt[offset].vt = VT_I4;
+      pvnt[offset].lVal = miniio;
+
+      offset++;
+    }
+
+    // Send User I/O
+    if (send_uio) {
+      pvnt[offset + 0].vt = VT_I4;
+      pvnt[offset + 0].lVal = send_userio_offset;
+
+      pvnt[offset + 1].vt = VT_I4;
+      pvnt[offset + 1].lVal = send_userio_size * USERIO_ALIGNMENT;
+
+      pvnt[offset + 2].vt = (VT_ARRAY | VT_UI1);
+      pvnt[offset + 2].parray = SafeArrayCreateVector(VT_UI1, 0, send_userio_size);
+      SafeArrayAccessData(pvnt[offset + 2].parray, (void**)&pbool);
+      memset(pbool, 0, send_userio_size * sizeof(uint8_t));
+      std::copy(send_userio.begin(), send_userio.end(), pbool);
+      SafeArrayUnaccessData(pvnt[offset + 2].parray);
+
+      offset += 3;
+    }
+
+    // Receive User I/O
+    if (recv_uio) {
+      pvnt[offset + 0].vt = VT_I4;
+      pvnt[offset + 0].lVal = recv_userio_offset;
+
+      pvnt[offset + 1].vt = VT_I4;
+      pvnt[offset + 1].lVal = recv_userio_size * USERIO_ALIGNMENT;
+
+      offset += 2;
+    }
+
+    // Hand I/O
+    if (send_hio) {
+      pvnt[offset].vt = VT_I4;
+      pvnt[offset].lVal = handio;
+
+      offset++;
+    }
+
+    SafeArrayUnaccessData(send->parray);
+  }
+
+  return S_OK;
+}
+
+HRESULT MedraDensoRobot::ParseRecvParameter(
+  const VARIANT_Ptr& recv, std::vector<double>& position,
+  std::vector<double>& joint, std::vector<double>& trans, int& miniio, int& handio,
+  int& timestamp, std::vector<uint8_t>& recv_userio, std::vector<double>& current)
+{
+  int type = m_recvfmt & SLVMODE_POSE;
+
+  // Check pose type
+  int j1 = 0, j2 = 0;
+  uint32_t joints = 0;
+  std::vector<double>*pose1 = NULL, *pose2 = NULL;
+
+  switch (type) {
+    case RECVFMT_POSE_P:
+      j1 = NUM_POSITION;
+      pose1 = &position;
+      break;
+    case RECVFMT_POSE_J:
+      j1 = NUM_JOINT;
+      pose1 = &joint;
+      break;
+    case RECVFMT_POSE_T:
+      j1 = NUM_TRANS;
+      pose1 = &trans;
+      break;
+    case RECVFMT_POSE_PJ:
+      j1 = NUM_POSITION;
+      j2 = NUM_JOINT;
+      pose1 = &position;
+      pose2 = &joint;
+      break;
+    case RECVFMT_POSE_TJ:
+      j1 = NUM_TRANS;
+      j2 = NUM_JOINT;
+      pose1 = &trans;
+      pose2 = &joint;
+      break;
+    default:
+      return E_FAIL;
+  }
+
+  joints = j1 + j2;
+
+  // Check receive format
+  bool recv_ts, recv_hio, recv_mio, recv_uio, recv_crt;
+  recv_ts = m_recvfmt & RECVFMT_TIME;
+  recv_hio = m_recvfmt & RECVFMT_HANDIO;
+  recv_mio = m_recvfmt & RECVFMT_MINIIO;
+  recv_uio = m_recvfmt & RECVFMT_USERIO;
+  recv_crt = m_recvfmt & RECVFMT_CURRENT;
+
+  // Number of arguments
+  uint32_t num = 1 + recv_ts + recv_hio + recv_mio + recv_uio + recv_crt;
+
+  HRESULT hr = S_OK;
+  VARIANT* pvnt;
+  double* pdbl;
+  uint8_t* pbool;
+
+  if (recv->vt == (VT_ARRAY | VT_R8)) {
+    if (joints != recv->parray->rgsabound->cElements) {
+      return E_FAIL;
+    }
+
+    // Pose only
+    SafeArrayAccessData(recv->parray, (void**)&pdbl);
+    pose1->resize(j1);
+    std::copy(pdbl, &pdbl[j1], pose1->begin());
+    if (pose2 != NULL) {
+      pose2->resize(j2);
+      std::copy(&pdbl[j1], &pdbl[joints], pose2->begin());
+    }
+    SafeArrayUnaccessData(recv->parray);
+  } else if (recv->vt == (VT_ARRAY | VT_VARIANT)) {
+    if (num != recv->parray->rgsabound->cElements) {
+      return E_FAIL;
+    }
+
+    SafeArrayAccessData(recv->parray, (void**)&pvnt);
+
+    int offset = 0;
+
+    // Timestamp
+    if (recv_ts) {
+      if (pvnt[offset].vt != VT_I4) {
+        hr = E_FAIL;
+        goto exit_proc;
+      }
+
+      timestamp = pvnt[offset].lVal;
+
+      offset++;
+    }
+
+    // Pose
+    {
+      if (
+        (pvnt[offset].vt != (VT_ARRAY | VT_R8)) || (joints != pvnt[offset].parray->rgsabound->cElements))
+      {
+        hr = E_FAIL;
+        goto exit_proc;
+      }
+
+      SafeArrayAccessData(pvnt[offset].parray, (void**)&pdbl);
+      pose1->resize(j1);
+      std::copy(pdbl, &pdbl[j1], pose1->begin());
+      if (pose2 != NULL) {
+        pose2->resize(j2);
+        std::copy(&pdbl[j1], &pdbl[joints], pose2->begin());
+      }
+      SafeArrayUnaccessData(pvnt[offset].parray);
+
+      offset++;
+    }
+
+    // Mini I/O
+    if (recv_mio) {
+      if (pvnt[offset].vt != VT_I4) {
+        hr = E_FAIL;
+        goto exit_proc;
+      }
+
+      miniio = pvnt[offset].lVal;
+
+      offset++;
+    }
+
+    // User I/O
+    if (recv_uio) {
+      if (pvnt[offset].vt != (VT_ARRAY | VT_UI1)) {
+        hr = E_FAIL;
+        goto exit_proc;
+      }
+
+      SafeArrayAccessData(pvnt[offset].parray, (void**)&pbool);
+      recv_userio.resize(pvnt[offset].parray->rgsabound->cElements);
+      std::copy(pbool, &pbool[pvnt[offset].parray->rgsabound->cElements], recv_userio.begin());
+      SafeArrayUnaccessData(pvnt[offset].parray);
+
+      offset++;
+    }
+
+    // Hand I/O
+    if (recv_hio) {
+      if (pvnt[offset].vt != VT_I4) {
+        hr = E_FAIL;
+        goto exit_proc;
+      }
+
+      handio = pvnt[offset].lVal;
+
+      offset++;
+    }
+
+    // Current
+    if (recv_crt) {
+      if (
+        (pvnt[offset].vt != (VT_ARRAY | VT_R8)) || (8 != pvnt[offset].parray->rgsabound->cElements))
+      {
+        hr = E_FAIL;
+        goto exit_proc;
+      }
+
+      SafeArrayAccessData(pvnt[offset].parray, (void**)&pdbl);
+      current.resize(8);
+      std::copy(pdbl, &pdbl[8], current.begin());
+      SafeArrayUnaccessData(pvnt[offset].parray);
+
+      offset++;
+    }
+
+    exit_proc:
+    SafeArrayUnaccessData(recv->parray);
+  } else {
+    return E_FAIL;
+  }
+
+  return hr;
 }
 
 
